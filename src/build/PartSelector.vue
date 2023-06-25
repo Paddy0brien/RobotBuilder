45f4666168989f14f7dbb3c74b70cdc547c75a27
Paddy0brien <patobrien1011@gmail.com>
2023-06-25 15:44:51 +1000

    Enabled inter-component communication.

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="part">
-      <img :src="selectedPart.src" title="arm"/>
+    <div class="part" :class="position">
+      <img :src="selectedPart.src" @click="showPartInfo = !showPartInfo" title="arm"/>
       <button @click="selectPreviousPart()" class="prev-selector"></button>
       <button @click="selectNextPart()" class="next-selector"></button>
       <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -8,9 +8,6 @@
 </template>
 
 <script>
-import availableParts from '../data/parts';
-
-const parts = availableParts.heads;
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -23,28 +20,49 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
+  props: {
+    parts: {
+      type: Array,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+      validator(value) {
+        return ['left', 'right', 'top', 'bottom', 'center'].includes(value);
+      },
+    },
+  },
   data() {
     return { selectedPartIndex: 0 };
   },
   computed: {
     selectedPart() {
-      return parts[this.selectedPartIndex];
+      return this.parts[this.selectedPartIndex];
     },
   },
+  created() {
+    this.emitSelectedPart();
+  },
+  updated() {
+    this.emitSelectedPart();
+  },
   methods: {
+    emitSelectedPart() {
+      this.$emit('partSelected', this.selectedPart);
+    },
     selectNextPart() {
       this.selectedPartIndex = getNextValidIndex(
         this.selectedPartIndex,
-        parts.length,
+        this.parts.length,
       );
     },
     selectPreviousPart() {
       this.selectedPartIndex = getPreviousValidIndex(
         this.selectedPartIndex,
-        parts.length,
+        this.parts.length,
       );
     },
-
   },
 };
 
